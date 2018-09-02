e43cd4e91aaa1b1bfb70a4c439fbbfb6c6edd316
BalazsParducz <parducz.balazs@gmail.com>
2018-09-02 12:54:51 +0200

    refactor ApiConnector to fulfil SoC design principles

--- src/main/java/com/codewarsapi/controller/ApiConnectorController.java
+++ src/main/java/com/codewarsapi/controller/ApiConnectorController.java
@@ -22,46 +22,29 @@ import java.util.List;
 @Controller
 public class ApiConnectorController {
 
-    public ApiService apiService = new ApiService();
-    private String codewars_user;
-    private final int pageOfRecentKatas = 0;
-
+    private ApiService apiService = new ApiService();
 
     @GetMapping("/")
     public String first(Model model) {
         model.addAttribute("codewars_user");
-//        codewars_user = model.addAttribute("codewars_user", new String()).toString();
         return "provide_user";
     }
 
     @PostMapping(value = "/getAPI")
     public String indexPage(@RequestParam("codewars_user") String codewars_user, Model model) {
         try {
-//            if(codewars_user==null) {
-//                throw new NullPointerException("Provide valid username.");
-//            }
-//            else {
-                JSONObject jsonFromMainAPI = getJSON(apiService.apiOf(codewars_user));
-                String userName = jsonFromMainAPI.getString("username");
-                String clan = jsonFromMainAPI.getString("clan");
-                int completedChallenges = jsonFromMainAPI.getJSONObject("codeChallenges").getInt("totalCompleted");
 
-                JSONObject jsonFromDetails = getJSON(apiService.apiOfCompletedChallengesFor(codewars_user));
-                String nameOfKata = jsonFromDetails.getJSONArray("data").getJSONObject(pageOfRecentKatas).get("name").toString();
-                String date = jsonFromDetails.getJSONArray("data").getJSONObject(pageOfRecentKatas).get("completedAt").toString();
-                SimpleDateFormat hungarianDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
-                Date timeOfCompletion = hungarianDateFormat.parse(date);
+                Date timeOfCompletion = apiService.getTimeOfCompletion(codewars_user);
 
-                List allKatas = getArrayOfKatas(codewars_user).toList();
+                List allKatas = apiService.getArrayOfKatas(codewars_user).toList();
 
-                model.addAttribute("username", userName);
-                model.addAttribute("clan", clan);
-                model.addAttribute("completedChallenges", completedChallenges);
+                model.addAttribute("username", apiService.getUserName(codewars_user));
+                model.addAttribute("clan", apiService.getUsersClan(codewars_user));
+                model.addAttribute("completedChallenges", apiService.getNrOfCompletedChallengesOf(codewars_user));
 
-                model.addAttribute("nameOfKata", nameOfKata);
+                model.addAttribute("nameOfKata", apiService.getNameOfKata(codewars_user));
                 model.addAttribute("timeOfCompletion", timeOfCompletion);
                 model.addAttribute("allKatas", allKatas);
-//            }
         } catch (IOException ioe) {
             model.addAttribute("error", "Unable to connect to Codewars' API.");        // log error
         } catch (ParseException pe) {
@@ -75,26 +58,26 @@ public class ApiConnectorController {
         return "index";
     }
 
-    private JSONArray getArrayOfKatas(String codewars_user) throws IOException {
-        JSONObject jsonFromDetails = getJSON(apiService.apiOfCompletedChallengesFor(codewars_user));
-        return jsonFromDetails.getJSONArray("data");
-    }
+//    private JSONArray getArrayOfKatas(String codewars_user) throws IOException {
+//        JSONObject jsonFromDetails = getJSON(apiService.apiOfCompletedChallengesFor(codewars_user));
+//        return jsonFromDetails.getJSONArray("data");
+//    }
 
 
-    private JSONObject getJSON(String urlString) throws IOException {
-        URL url = new URL(urlString);                                           // 1) URL megadása
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();       // 2) kapcsolódás
-        con.setRequestMethod("GET");                                        // 3) Request method megadás
-        BufferedReader in = new BufferedReader(                         // 4) Tartalom beolvasása
-                new InputStreamReader(con.getInputStream()));
-        String inputLine;                                               // 5) Segéd változó
-        StringBuffer content = new StringBuffer();                      // 6) Tartalom változó létrehozása
-        while ((inputLine = in.readLine()) != null) {                   // 7) A Tartalom változó "feltöltése" while-lal
-            content.append(inputLine);
-        }
-        in.close();                                                     // 8)  a BufferedReader bezárása - szükséges
-        String returnedFromCodewarsAPI = content.toString();            // 9) Segédváltozó: a vissatérő String egésze
-        return new JSONObject(returnedFromCodewarsAPI);           // 10) JSON-ná alakítás
-    }
+//    private JSONObject getJSON(String urlString) throws IOException {
+//        URL url = new URL(urlString);                                           // 1) URL megadása
+//        HttpURLConnection con = (HttpURLConnection) url.openConnection();       // 2) kapcsolódás
+//        con.setRequestMethod("GET");                                        // 3) Request method megadás
+//        BufferedReader in = new BufferedReader(                         // 4) Tartalom beolvasása
+//                new InputStreamReader(con.getInputStream()));
+//        String inputLine;                                               // 5) Segéd változó
+//        StringBuffer content = new StringBuffer();                      // 6) Tartalom változó létrehozása
+//        while ((inputLine = in.readLine()) != null) {                   // 7) A Tartalom változó "feltöltése" while-lal
+//            content.append(inputLine);
+//        }
+//        in.close();                                                     // 8)  a BufferedReader bezárása - szükséges
+//        String returnedFromCodewarsAPI = content.toString();            // 9) Segédváltozó: a vissatérő String egésze
+//        return new JSONObject(returnedFromCodewarsAPI);           // 10) JSON-ná alakítás
+//    }
 
 }
--- src/main/java/com/codewarsapi/service/ApiService.java
+++ src/main/java/com/codewarsapi/service/ApiService.java
@@ -4,16 +4,76 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.stereotype.Component;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @Component
 public class ApiService {
 
+    private final int pageOfRecentKatas = 0;
+
+
     public String apiOfCompletedChallengesFor(String username) {
         return "https://www.codewars.com/api/v1/users/"+ username + "/code-challenges/completed?page=0";
     }
 
+    private JSONObject getJSON(String urlString) throws IOException {
+        URL url = new URL(urlString);                                           // 1) URL megadása
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();       // 2) kapcsolódás
+        con.setRequestMethod("GET");                                        // 3) Request method megadás
+        BufferedReader in = new BufferedReader(                         // 4) Tartalom beolvasása
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;                                               // 5) Segéd változó
+        StringBuffer content = new StringBuffer();                      // 6) Tartalom változó létrehozása
+        while ((inputLine = in.readLine()) != null) {                   // 7) A Tartalom változó "feltöltése" while-lal
+            content.append(inputLine);
+        }
+        in.close();                                                     // 8)  a BufferedReader bezárása - szükséges
+        String returnedFromCodewarsAPI = content.toString();            // 9) Segédváltozó: a vissatérő String egésze
+        return new JSONObject(returnedFromCodewarsAPI);           // 10) JSON-ná alakítás
+    }
+
     public String apiOf(String username) {
         return "https://www.codewars.com/api/v1/users/" + username;
+    }
+
+    public JSONArray getArrayOfKatas(String codewars_user) throws IOException {
+        JSONObject jsonFromDetails = getJSON(apiOfCompletedChallengesFor(codewars_user));
+        return jsonFromDetails.getJSONArray("data");
+    }
+
+    public Date getTimeOfCompletion(String codewars_user) throws IOException, ParseException {
+        JSONObject jsonFromDetails = getJSON(apiOfCompletedChallengesFor(codewars_user));
+        String date = jsonFromDetails.getJSONArray("data").getJSONObject(pageOfRecentKatas).get("completedAt").toString();
+        SimpleDateFormat hungarianDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+        Date timeOfCompletion = hungarianDateFormat.parse(date);
+        return timeOfCompletion;
+    }
+
+    public String getNameOfKata(String codewars_user) throws IOException {
+        String nameOfKata = getJSON(apiOfCompletedChallengesFor(codewars_user)).getJSONArray("data").getJSONObject(pageOfRecentKatas).get("name").toString();
+        return nameOfKata;
+    }
+
+    public String getUserName(String codewars_user) throws IOException {
+        String userName = getJSON(apiOf(codewars_user)).getString("username");
+        return userName;
+    }
+
+    public String getUsersClan(String codewars_user) throws IOException {
+        String clan = getJSON(apiOf(codewars_user)).getString("clan");
+        return clan;
+    }
+
+    public int getNrOfCompletedChallengesOf(String codewars_user) throws IOException {
+        int completedChallenges = getJSON(apiOf(codewars_user)).getJSONObject("codeChallenges").getInt("totalCompleted");
+        return completedChallenges;
     }
 }
