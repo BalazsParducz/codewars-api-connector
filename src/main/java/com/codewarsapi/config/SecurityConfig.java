ac6822b8c7aa5103893588afc993e15424158494
BalazsParducz <parducz.balazs@gmail.com>
2018-09-17 12:04:11 +0200

    basic authentication

---
+++ src/main/java/com/codewarsapi/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.codewarsapi.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableGlobalMethodSecurity(securedEnabled = true)
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Autowired
+    public void configureAuth(AuthenticationManagerBuilder auth) throws Exception {
+
+
+            auth
+                .inMemoryAuthentication()
+                    .withUser("B-dog")
+                    .password("hungary")
+                    .roles("USER");
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity httpSec) throws Exception {
+            httpSec
+                    .authorizeRequests()
+                        .antMatchers(HttpMethod.GET, "/").permitAll()
+                        .antMatchers("/delete").hasRole("ADMIN")
+                        .antMatchers("/admin/**").hasRole("ADMIN")
+                        .and()
+                        .formLogin().permitAll();
+    }
+
+}
