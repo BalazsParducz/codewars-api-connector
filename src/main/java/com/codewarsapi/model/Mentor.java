6d03f882ac17ee43dc910627932ba3fdd07af6ba
BalazsParducz <parducz.balazs@gmail.com>
2018-09-17 16:29:28 +0200

    POJO for registration

---
+++ src/main/java/com/codewarsapi/model/Mentor.java
@@ -0,0 +1,31 @@
+package com.codewarsapi.model;
+
+public class Mentor {
+
+    private String username;
+    private String password;
+
+    public Mentor() {}
+
+    public Mentor(String username, String password) {
+        super();
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
