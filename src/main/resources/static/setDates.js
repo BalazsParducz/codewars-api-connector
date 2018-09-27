16c098ca460d397aeabf4d8a529ad81ee31d0d90
BalazsParducz <parducz.balazs@gmail.com>
2018-09-27 19:50:54 +0200

    implemented button to set dates for two weeks period

---
+++ src/main/resources/static/setDates.js
@@ -0,0 +1,12 @@
+
+let now = new Date();
+let twoWeeksEarlier = now.getTime()-14*24*60*60*1000;
+let twoWeeksAgo = new Date(twoWeeksEarlier);
+
+function setDate() {
+    document.getElementById("from").value =twoWeeksAgo.getFullYear() +'-'+(twoWeeksAgo.getMonth()+1).toString().padStart(2, "0")+'-'+twoWeeksAgo.getDate();
+    document.getElementById("to").value= now.getFullYear() +'-'+(now.getMonth()+1).toString().padStart(2, "0")+'-'+now.getDate();
+}
+
+document.getElementById("setDate").onclick = setDate;
+
