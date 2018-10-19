1ddc26fc827da2ed1d22749ffdb85b997874919e
BalazsParducz <parducz.balazs@gmail.com>
2018-10-19 15:35:24 +0200

    modified returned date strings so that format does not brake when months or days are made of single character

--- src/main/resources/static/setDates.js
+++ src/main/resources/static/setDates.js
@@ -4,8 +4,8 @@ let twoWeeksEarlier = now.getTime()-14*24*60*60*1000;
 let twoWeeksAgo = new Date(twoWeeksEarlier);
 
 function setDate() {
-    document.getElementById("from").value =twoWeeksAgo.getFullYear() +'-'+(twoWeeksAgo.getMonth()+1).toString().padStart(2, "0")+'-'+twoWeeksAgo.getDate();
-    document.getElementById("to").value= now.getFullYear() +'-'+(now.getMonth()+1).toString().padStart(2, "0")+'-'+now.getDate();
+    document.getElementById("from").value =twoWeeksAgo.getFullYear() +'-'+(twoWeeksAgo.getMonth()+1).toString().padStart(2, "0")+'-'+twoWeeksAgo.getDate().toString().padStart(2, "0");
+    document.getElementById("to").value= now.getFullYear() +'-'+(now.getMonth()+1).toString().padStart(2, "0")+'-'+now.getDate().toString().padStart(2, "0");
 }
 
 document.getElementById("setDate").onclick = setDate;
